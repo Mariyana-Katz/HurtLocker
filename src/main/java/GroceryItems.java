c23a93e79cef30199f0c8801b66cd31c1df42f18
Mariyana-Katz <mariqnak21@gmail.com>
2024-04-19 08:53:12 -0400

    some work done

---
+++ src/main/java/GroceryItems.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+
+public class GroceryItems {
+
+    String name;
+    String type;
+    Double price;
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -10,8 +12,47 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception{
+        //get text from the rawDataFile
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+        String parsedJSON = (new Main()).parseToJsonString(output);
+        System.out.println("The Parsed JSON: " + parsedJSON);
+    }
 
+    public String parseToJsonString(String input)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"items\": [");
+
+        //split into group of items
+        Pattern patternGroups = Pattern.compile("(.*?)##");
+        Matcher matcherGroups = patternGroups.matcher(input);
+
+        while (matcherGroups.find()) {
+            sb.append("{");
+            String group = matcherGroups.group(1);
+            //System.out.println("Group: " + group);
+
+            //parse each key-value pair
+            Pattern pattern = Pattern.compile("\\b(\\w+):([a-zA-Z_0-9/.]+)\\b");
+            Matcher matcher = pattern.matcher(group);
+
+            while (matcher.find()) {
+                String key = matcher.group(1);
+                String value = matcher.group(2);
+                sb.append("\"").append(key).append("\":\"").append(value).append("\",");
+
+                //System.out.println(key + ':' + value);
+            }
+            sb.append("},");
+        }
+        sb.append("]");
+        sb.append("}");
+        return sb.toString();
     }
 }
+
+
+
+
